ad1ba53e552e8f4d581856c37d44877c16169174
albim72 <marcin.albiniak72@gmail.com>
2025-06-30 12:15:55 +0200

    Create listy_operacje.py

---
+++ DZIEN_1/listy_operacje.py
@@ -0,0 +1,12 @@
+teksty = ["Anna","Tymek","Marek","Kasia","Krzysztof","Michał","Krzysztof","Krzysztof","Leon"]
+
+print(teksty)
+
+wycinek = teksty[2:5] #wycinek - slice
+print(wycinek)
+print(teksty[6])
+print(teksty[:4])
+print(teksty[3:])
+print(teksty[-1])
+print(teksty[::2]) #pozycje parzyste
+print(teksty[1::2]) #pozycje nieparzyste
