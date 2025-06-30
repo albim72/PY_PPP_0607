121e11b4cf3b3a70fb3a94626a7f0b6d4c30ad8b
albim72 <marcin.albiniak72@gmail.com>
2025-06-30 13:32:10 +0200

    Update listy_operacje.py

--- DZIEN_1/listy_operacje.py
+++ DZIEN_1/listy_operacje.py
@@ -10,3 +10,62 @@ print(teksty[3:])
 print(teksty[-1])
 print(teksty[::2]) #pozycje parzyste
 print(teksty[1::2]) #pozycje nieparzyste
+#odwrócmy listę -> list[start:stop:krok]
+print(teksty[::-1])
+
+teksty.reverse() #zamiana listy
+
+print(teksty)
+
+tk = list(teksty)
+tk.sort()#sortowanie listy
+print(teksty)
+print(tk)
+
+#tworzenie kopii
+import copy
+
+l1 = [[1,2],[3,4]]
+l2 = copy.copy(l1)
+# l1[0] = [2,7]
+# l1[0][1] = 90
+# l2[1] = [3,3]
+l2[1][1] = 100 #kopia copy nie  blokuje w wewnętrznych listach
+print(l1)
+print(l2)
+
+#kopia głęboka
+listaR = [[1,2],[3,4],[5,6]]
+
+listaR2 = copy.deepcopy(listaR)
+listaR2[1][1] = 100
+
+print(listaR)
+print(listaR2)
+
+hero = "superman"
+
+print(hero)
+
+print(hero[1:3])
+print(hero[:4])
+print(hero[4:])
+print(hero[::-1])
+
+print(hero.upper())
+print(hero.replace("m","n"))
+print(len(hero))
+
+#operacje tekstowe na liście
+duze = [tekst.upper() for tekst in teksty] #list comprehension
+print(duze)
+
+#zamiana liter w liście
+zamienione = [imie.replace("a","e") for imie in teksty]
+print(zamienione)
+
+#filtrowanie
+filtr = [imie for imie in teksty if imie.startswith(("K","A"))]
+print(filtr)
+
+
