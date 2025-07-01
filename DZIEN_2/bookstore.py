2478558e9144381ade5c5c51d5feb3da0eb48aae
albim72 <marcin.albiniak72@gmail.com>
2025-07-01 11:57:43 +0200

    Update bookstore.py

--- DZIEN_2/bookstore.py
+++ DZIEN_2/bookstore.py
@@ -7,3 +7,15 @@ class Book:
         self.price = float(price)
         self.read_pages = 0
         self.finished = False
+
+    def read(self, pages):
+        if self.finished:
+            print(f"skończyłeś czytac książkę {self.title}")
+            return
+        self.read_pages += pages
+        if self.read_pages >= self.pages:
+            self.read_pages = self.pages    
+            self.finished = True
+            print(f"zakończyłeś czytac książkę {self.title}")
+        else:
+            print(f"czytasz {pages} stronę książki. Przeczytano {(self.read_pages/self.pages)*100}% stron")
