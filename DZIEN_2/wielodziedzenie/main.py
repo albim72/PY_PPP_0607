df5cd8ca600087cf14988bcec8da7282ea26b66e
albim72 <marcin.albiniak72@gmail.com>
2025-07-01 14:08:37 +0200

    Update main.py

--- DZIEN_2/wielodziedzenie/main.py
+++ DZIEN_2/wielodziedzenie/main.py
@@ -1,3 +1,17 @@
 from redundancja import Child
+from konflikt_nazw import C
+from abstrakcja import Abstract,Uchwyt
 
 c= Child()
+
+print("******************")
+c = C()
+c.hello()
+
+print("******************")
+print(C.__mro__)
+
+print("******************")
+# a = Abstract() #nie powinno się tworzyc obiektów klasy abstrakcyjnej
+u = Uchwyt()
+
